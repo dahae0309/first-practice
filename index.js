d4decd3b5aee37485055696b5512f9b921f3b782
Das Kang <>
2023-02-15 19:13:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function removeAll(source, pattern){
+    if(pattern.length === 0){
+    return source;
+    }
+	if(source.includes(pattern[0])){
+    source = source.replace(pattern[0], '')
+  }
+return removeAll(source, pattern.slice(1))
+}
+
+console.log(removeAll("ImPossible", "Im"))
